f0fdcdc6f4ea0145cc2fc1e7c5c5e003b900e380
Rdj23 <rohan.jadhav@clevertap.com>
2025-08-31 18:52:06 +0530

    click listner

--- android/app/src/main/kotlin/com/example/clevertap_push_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/clevertap_push_demo/MainActivity.kt
@@ -2,4 +2,14 @@ package com.example.clevertap_push_demo
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        // Handle notification clicks on Android 12+ explicitly
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            CleverTapAPI.getDefaultInstance(applicationContext)
+                ?.pushNotificationClickedEvent(intent?.extras)
+        }
+    }
+}
